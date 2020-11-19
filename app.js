f4701582e7ebfbfec768b27e7a6925e199d14a1b
Riddik96 <vladi96@hotmail.it>
2020-11-19 15:33:53 +0100

    Import mongoose

--- app.js
+++ app.js
@@ -3,11 +3,14 @@ const {graphqlHTTP} = require('express-graphql');
 const schema = require('./graphql/schema');
 const resolvers = require('./graphql/resolvers');
 const mongoose = require('mongoose');
+
 const app = express();
-const db = mongoose.connect('mongodb+srv://animeAdmin:animeAdmin9000@cluster0.bqbxg.gcp.mongodb.net/anime9000?retryWrites=true&w=majority')
+const db = await mongoose.connect('mongodb+srv://animeAdmin:animeAdmin9000@cluster0.bqbxg.gcp.mongodb.net/anime9000?retryWrites=true&w=majority')
+
 app.use('/graphql', graphqlHTTP({
     schema: schema,
     rootValue: resolvers,
+    context: db,
     graphiql: true,
 }));
 
