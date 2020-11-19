1df03c1db26f8b71d6922e7272f40502c4506f8b
Riddik96 <vladi96@hotmail.it>
2020-11-19 10:47:24 +0100

    Added first schema

--- app.js
+++ app.js
@@ -1,6 +1,27 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
+var {graphqlHTTP} = require('express-graphql');
+var {buildSchema} = require('graphql');
+
+var schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
+
+const root = {
+    hello: () => {
+        return 'Hello world!';
+    },
+};
+
 const app = express();
+
+app.use('/graphql', graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true,
+}));
 
 app.use('/api/graph', graphqlHTTP({}));
 
